5a4dd6ee307872b2b8dc5e090fea7f7e326c29f8
Er. Jawwad Kazi <42866363+ProfessionalJK@users.noreply.github.com>
2020-08-27 15:57:20 +0530

    Updates

---
+++ precache-manifest.409446158d366579d806a3b7731afca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165c5b6dc50ca24cf2fac15ecbd14d24",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "85ee8d17afcfd2e9183c",
+    "url": "/todo-app/static/css/main.037b396f.chunk.css"
+  },
+  {
+    "revision": "f67da48931c512b5e899",
+    "url": "/todo-app/static/js/2.60e3458b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.60e3458b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ee8d17afcfd2e9183c",
+    "url": "/todo-app/static/js/main.27e0977f.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
